7d2b0fc5e49d1e496ea4130e79f0bdae7b64727d
rupak-sarkar <rupak.sar@gmail.com>
2025-10-10 01:07:52 +0530

    Initial commit for xchart.in

---
+++ app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.set_page_config(page_title="XChart", layout="wide")
+st.title("📈 Welcome to XChart.in")
+st.write("Your hub for algorithmic trading insights and smart investing strategies.")
